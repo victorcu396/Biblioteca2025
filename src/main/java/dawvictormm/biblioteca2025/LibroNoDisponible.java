7de6b087a3050264e5bb83b59b5f6d2b96d1bff3
Victor <vmenendezmata@gmail.com>
2025-01-27 10:23:33 +0100

    Seguimos ahora con excepciones

---
+++ src/main/java/dawvictormm/biblioteca2025/LibroNoDisponible.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dawvictormm.biblioteca2025;
+
+/**
+ *
+ * @author alu16d
+ */
+public class LibroNoDisponible extends Exception{
+    public LibroNoDisponible (String cadena) {
+        super(cadena);
+    }
+}
